95bb8e4269fe1e03ceb0b04107cdbee8be39705b
illya.ustenko <illya.ustenko@hva.nl>
2021-07-21 21:37:37 +0200

    added second ADC port A1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import board
 import busio
+import csv
 
 # Voor nu van Adafruit binnenkort even zelf aanpassen
 import adafruit_ads1x15.ads1015 as ADS
@@ -13,13 +14,14 @@ i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1015(i2c)
 
 # Create single-ended input on channel 0
-chan = AnalogIn(ads, ADS.P0)
+currentClamp = AnalogIn(ads, ADS.P0)
+vibrationSensor = AnalogIn(ads, ADS.P1)
 
-print("{:>5}\t{:>5}".format("raw", "v"))
+print("{:>5}\t{:>5}\t{:>5}\t{:>5}".format("raw A0", "v A0", "raw A1", "v A1"))
 
 while True:
-    print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
-    time.sleep(0.5)
+    print("{:>5}\t{:>5.3f}\t{:>5}\t{:>5.3f}".format(currentClamp.value, currentClamp.voltage, vibrationSensor.value, vibrationSensor.voltage))
+    time.sleep(0.2)
 
 
 
