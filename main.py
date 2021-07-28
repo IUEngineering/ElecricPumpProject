f55e3ebb52c1ee13146b0dbeb3616be85aefa0d8
illya.ustenko <illya.ustenko@hva.nl>
2021-07-28 23:58:51 +0200

    final for 28/7

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import board
 import busio
 import csv
-from time import sleep, strftime, time
-from gpiozero import CPUTemperature
-
-# Create CPU object
-cpu = CPUTemperature()
-
+import time
 
 # Voor nu van Adafruit binnenkort even zelf aanpassen
 import adafruit_ads1x15.ads1015 as ADS
@@ -22,25 +17,14 @@ writer = csv.writer(f)
 i2c = busio.I2C(board.SCL, board.SDA)
 
 # Create an ADC object using the I2C bus
-ads = ADS.ADS1015(i2c)
+ads = ADS.ADS1015(i2c, data_rate=3300)
 
 # Create single-ended input on channel 0
-currentClamp = AnalogIn(ads, ADS.P0)
-vibrationSensor = AnalogIn(ads, ADS.P1)
+currentClamp = AnalogIn(ads, ADS.P0, ADS.P1)
+vibrationSensor = AnalogIn(ads, ADS.P2)
 
-# print("{:>5}\t{:>5}\t{:>5}\t{:>5}".format("raw A0", "v A0", "raw A1", "v A1"))
-
-# while True:
-#     print("{:>5}\t{:>5.3f}\t{:>5}\t{:>5.3f}".format(currentClamp.value, currentClamp.voltage, vibrationSensor.value, vibrationSensor.voltage))
-#
-#
-#     #time.sleep(0.2)
-
-with open("/mnt/data/testing/cpu_temp.csv", "a") as log:
+with open("/mnt/data/testing/test_sleep.csv", "a") as log:
     while True:
-        temp = cpu.temperature
-        log.write("{0},{1},{2},{3}\n".format(strftime("%Y-%m-%d %H:%M:%S"), str(temp), currentClamp.value, vibrationSensor.value))
-        sleep(1)
-
-
-
+        log.write("{0}\n".format(currentClamp.value))
+        # log.write("{0},{1},{2},{3}\n".format(strftime("%Y-%m-%d %H:%M:%S"), str(temp), currentClamp.value, vibrationSensor.value))
+        # log.write("{0}\n", currentClamp.value)
