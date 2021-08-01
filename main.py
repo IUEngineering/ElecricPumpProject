dc9ebc2f6c80e5179d160a50243ecc24ee346c90
illya.ustenko <illya.ustenko@hva.nl>
2021-08-01 14:47:49 +0200

    Final version, removed all crappy code.

--- main.py
+++ main.py
@@ -3,24 +3,16 @@
 
 import board
 import busio
-import csv
 from datetime import datetime
 import time
 import os
-# Voor nu van Adafruit binnenkort even zelf aanpassen
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 from adafruit_ads1x15.ads1115 import Mode
 from namechanger import newName
 from pytz import timezone
 
-
-# # open the file in the write mode
-# f = open('/mnt/data/', 'w')
-#
-# # create the csv writer
-# writer = csv.writer(f)
-
+# Mounten van de harde schrijf op de Raspberry PI
 os.system("sudo mount -U 12E2C15FE2C14825 /mnt/SSDdata")
 
 # Create an I2C bus
@@ -47,10 +39,12 @@ def logger():
     with open(newName("/mnt/SSDdata/data.csv"), "a") as log:
         print("1")
         log.write("{0},{1}\n".format("START", datetime.now(timezone('UTC')).astimezone(timezone('Europe/Berlin'))))
+        # Zippen was mislukt maar dit is hoe je het zou kunnen doen
         #zipFilesInDir("/mnt/data", "/mnt/usb1/deZipMetData.zip", lambda name: 'csv' in name)
         # time in seconds, 1440 = 24 hours
         while time.time() - start_time <= 86400:
             log.write("{0},{1}\n".format(currentClamp.voltage, vibrationSensor.voltage))
+
             # You can enable print for testing purposes
             # print(currentClamp
             # print(vibrationSensor.voltage)
